e4f47a289b1c97669b808f8d762853456cf17266
gagan <gagsh1812@gmail.com>
2020-03-12 12:55:10 +0530

    refactor

--- src/components/game.js
+++ src/components/game.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import {Modal} from 'antd';
+import {
+  Modal
+} from 'antd';
 import Quiz from './quiz';
 import Header from './header';
 
-class Game extends React.Component{
+class Game extends React.Component {
 
   _isMounted = false;
-  
-  constructor(props){
+
+  constructor(props) {
     super(props);
-    
+
     this.state = {
-      isGameOver:false,
+      isGameOver: false,
       score: 0,
       time: 0,
       countdown: 5,
@@ -21,46 +23,46 @@ class Game extends React.Component{
 
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this._isMounted = true;
     this.startTimer();
   }
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this._isMounted = false;
     clearInterval(this.counter)
   }
-  
-  startTimer = (changeState=true)=>{
-    if(this.state.countdown===0 && changeState){
+
+  startTimer = (changeState = true) => {
+    if (this.state.countdown === 0 && changeState) {
       clearInterval(this.counter)
       this.setState({
-        isGameOver:true,
+        isGameOver: true,
       })
-    }
-    else this.counter = setInterval(this.timer, 1000)
-     
+    } else this.counter = setInterval(this.timer, 1000)
+
   }
 
 
-  timer = ()=> {
-    this.setState((prevState)=>({
+  timer = () => {
+    this.setState((prevState) => ({
       updateQuiz: false,
-      countdown: prevState.countdown-1,
-      time : prevState.time + 1,
-    }
-    ))
-    if(this.state.countdown===0){
-      clearInterval(this.counter)
-      this.setState({
-        isGameOver:true,
-      })
-    }
+      countdown: prevState.countdown - 1,
+      time: prevState.time + 1,
+    }), () => {
+      if (this.state.countdown === 0) {
+        clearInterval(this.counter)
+        this.setState({
+          isGameOver: true,
+        })
+      }
+    })
+
   }
 
   reset = () => {
     this.setState({
-      isGameOver:false,
+      isGameOver: false,
       countdown: 5,
       time: 0,
       score: 0,
@@ -69,21 +71,20 @@ class Game extends React.Component{
     this.startTimer(false);
   }
 
-  onAnswerClick = (clickedOption, answer)=>{
-    if(clickedOption===answer){
+  onAnswerClick = (clickedOption, answer) => {
+    if (clickedOption === answer) {
       clearInterval(this.counter)
-      
-      this.setState((prevState)=>({
+
+      this.setState((prevState) => ({
         isGameOver: false,
-        score : prevState.score+1,
-        updateQuiz : true,
-        time : prevState.time,
+        score: prevState.score + 1,
+        updateQuiz: true,
+        time: prevState.time,
         countdown: 5,
-        level : prevState.score < 5 ? 1 : (prevState.score <10 ? 2 : 3),
+        level: prevState.score < 5 ? 1 : (prevState.score < 10 ? 2 : 3),
       }));
       this.startTimer(false);
-    }
-    else{
+    } else {
       clearInterval(this.counter)
       this.setState({
         isGameOver: true,
@@ -91,27 +92,57 @@ class Game extends React.Component{
     }
   }
 
-  render(){
-    return (
-      <div>
-        {!this.state.isGameOver && 
-        <div>
-        <Header countdown= {this.state.countdown} time = {this.state.time} score = {this.state.score}/>
-        <Quiz onAnswerClick = {this.onAnswerClick} update = {this.state.updateQuiz} level = {this.state.level} />
-        </div>
-     
-            }      
-      <Modal
-          title="Game Over"
-          visible={this.state.isGameOver}
-          onOk={this.reset}
-          onCancel={this.reset}
-      >
-        <p>Total time taken: {this.state.time}</p>
-        <p>Score: {this.state.score}</p>
-      </Modal>
-      
-    </div>
+  render() {
+    return ( <
+      div > {
+        !this.state.isGameOver &&
+        <
+        div >
+        <
+        Header countdown = {
+          this.state.countdown
+        }
+        time = {
+          this.state.time
+        }
+        score = {
+          this.state.score
+        }
+        /> <
+        Quiz onAnswerClick = {
+          this.onAnswerClick
+        }
+        update = {
+          this.state.updateQuiz
+        }
+        level = {
+          this.state.level
+        }
+        /> <
+        /div>
+
+      } <
+      Modal title = "Game Over"
+      visible = {
+        this.state.isGameOver
+      }
+      onOk = {
+        this.reset
+      }
+      onCancel = {
+        this.reset
+      } >
+      <
+      p > Total time taken: {
+        this.state.time
+      } < /p> <
+      p > Score: {
+        this.state.score
+      } < /p> <
+      /Modal>
+
+      <
+      /div>
     )
   }
 }
